9f2d3193dba8090e4be309bc3da57573b2a6cc98
Gustavo Lemos <contato.gustavolemos@outlook.com>
2025-03-20 01:31:08 -0300

    aula 03 - rotas aninhadas

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,44 @@
+import styles from "./SobreMim.module.css";
+
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png";
+import fotoSobreMim from "assets/sobre_mim_foto.png";
+
 export default function SobreMim() {
     return (
-        <h1>Sobre mim...</h1>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+        >
+            <h3 className={styles.subtitulo}>
+                Olá, eu sou o Gustavo!
+            </h3>
+
+            <img
+                src={fotoSobreMim}
+                alt="Foto do Gustavo"
+                className={styles.fotoSobreMim}
+            />
+
+            <p className={styles.paragrafo}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className={styles.paragrafo}>
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <p className={styles.paragrafo}>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.
+            </p>
+            <p className={styles.paragrafo}>
+                Pellentesque in libero id felis ultrices scelerisque. Proin venenatis magna in lorem consequat, eget facilisis lectus suscipit.
+            </p>
+            <p className={styles.paragrafo}>
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer eu neque id velit convallis interdum.
+            </p>
+            <p className={styles.paragrafo}>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+            </p>
+
+        </PostModelo>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicial from "./paginas/Inicial";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
     <Menu />
       <Routes>
-        <Route path="/" element={<Inicial />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicial />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
+
         <Route path="*" element={<div>Página não encontrada!</div>}/>
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
