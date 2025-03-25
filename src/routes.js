01aca23f8d5cbe697adb99ad6d12e48d50061ad2
Gustavo Lemos <contato.gustavolemos@outlook.com>
2025-03-25 00:27:24 -0300

    pequenos ajustes

--- src/routes.js
+++ src/routes.js
@@ -6,21 +6,22 @@ import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "paginas/Post";
 import NaoEncontrada from "paginas/NaoEncontrada";
+import SubirPagina from "./componentes/SubirPagina/index";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
     <Menu />
+    <SubirPagina /> 
       <Routes>
-        <Route path="/" element={<PaginaPadrao />}>
-          <Route index element={<Inicial />} />
-          <Route path="sobremim" element={<SobreMim />} />
-        </Route>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicial />} />
+            <Route path="sobremim" element={<SobreMim />} />
+          </Route>
 
-        <Route path="posts/:id/*" element={<Post />} />
-        <Route path="*" element={<NaoEncontrada />}/>
-      </Routes>
-
+          <Route path="posts/:id/*" element={<Post />} />
+          <Route path="*" element={<NaoEncontrada />}/>
+        </Routes>
       <Rodape />
     </BrowserRouter>
   );
